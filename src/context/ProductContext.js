8b35c709bc97172aab6196d01d8535b8a88186d1
NizarSH98 <jabernizar98@gmail.com>
2024-07-20 17:21:27 +0300

    fix

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -11,12 +11,12 @@ export const ProductProvider = ({ children }) => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const apiUrl = `https://tijar-7822a.web.app/categories`;
+        const apiUrl = '/db.json';
         console.log('Fetching from URL:', apiUrl);  // Debugging log
         const response = await axios.get(apiUrl);
         console.log('API Response:', response.data);  // Debugging log
-        if (Array.isArray(response.data)) {
-          setCategories(response.data);
+        if (response.data && Array.isArray(response.data.categories)) {
+          setCategories(response.data.categories);
         } else {
           console.error('Expected an array but got:', response.data);
         }
