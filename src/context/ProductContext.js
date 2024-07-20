bffebda24eee379a348f6f9000e5c0acc6205c7e
NizarSH98 <jabernizar98@gmail.com>
2024-07-20 17:14:00 +0300

    fix

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -11,7 +11,7 @@ export const ProductProvider = ({ children }) => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const apiUrl = `${process.env.REACT_APP_API_URL}/categories`;
+        const apiUrl = `https://tijar-7822a.web.app/categories`;
         console.log('Fetching from URL:', apiUrl);  // Debugging log
         const response = await axios.get(apiUrl);
         console.log('API Response:', response.data);  // Debugging log
